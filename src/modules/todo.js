c06a73547cfce651d27f6b843a27fd451b194af7
Liam <liargent@hotmail.com>
2023-01-10 21:44:17 +0000

    Add code to allow for completion of to do items

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,14 +1,20 @@
+import { publish } from './pubsub';
+
 export default class ToDo {
+  #isCompleted;
+
   constructor(
     title = 'New Item',
     dueDate = new Date(),
     description = '',
-    priority = 'normal'
+    priority = 'normal',
+    isCompleted = false
   ) {
     this.title = title;
     this.dueDate = this.extractDateFromDateInputString(dueDate);
     this.priority = priority;
     this.description = description;
+    this.#isCompleted = isCompleted;
   }
 
   extractDateFromDateInputString(string) {
@@ -19,5 +25,14 @@ export default class ToDo {
 
   get isDue() {
     return this.dueDate.getTime() <= Date.now();
+  }
+
+  get completed() {
+    return this.#isCompleted;
+  }
+
+  set completed(value) {
+    this.#isCompleted = value;
+    publish('onToDoChange', this);
   }
 }
